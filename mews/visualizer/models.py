01cf28c9b89ba8e8a27daab59d70a5cdc2856fa9
Kelvin <kelvindo@stanford.edu>
2015-05-03 16:15:59 -0700

    added fake data to db and listing articles now

--- mews/visualizer/models.py
+++ mews/visualizer/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=200)
     URL = models.URLField(max_length=200)
+    abstract = models.TextField()
--- mews/visualizer/views.py
+++ mews/visualizer/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 
 # Create your views here.
 def index(request):
-    context = {}
+    articles = Article.objects.all()
+    context = {'articles': articles}
 
     return render(request, 'visualizer/index.html', context)
