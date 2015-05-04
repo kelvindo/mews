54d4d365b8078b2089e39e262e773a4ed4504ee5
Kelvin <kelvindo@stanford.edu>
2015-05-04 16:56:33 -0700

    Added bomb map

--- mews/visualizer/urls.py
+++ mews/visualizer/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    #/ - landing page. currently displays list of articles
     url(r'^$', views.index, name='index'),
 
+    #/map - shows the map
+    url(r'^map$', views.map, name='map'),
     ]
--- mews/visualizer/views.py
+++ mews/visualizer/views.py
@@ -5,10 +5,12 @@ import getNewsNYT as NYTNews
 
 # Create your views here.
 def index(request):
-
     locations = Location.objects.all()
-    print locations
     articles = NYTNews.getTopStoriesNYT()
     context = {'articles': articles, 'locations': locations}
 
     return render(request, 'visualizer/index.html', context)
+
+def map(request):
+    context = {}
+    return render(request, 'visualizer/map.html', context)
