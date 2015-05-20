d39d422148844b6a6d7dce1a029623455bbc537f
Kevin Lu <klu1@stanford.edu>
2015-05-20 15:23:30 -0700

    added section map

--- mews/visualizer/views.py
+++ mews/visualizer/views.py
@@ -23,9 +23,8 @@ def map(request):
     return render(request, 'visualizer/map.html', context)
 
 def mapWithFilters(request):
-    topArticles = NYTNews.getTopNYT()
-    mostViewedArticles = NYTNews.getMostViewedNYT('all-sections', 1)
-    articlesBySection = aFilter.filterBySection(topArticles)
+    mostViewedArticles = NYTNews.getMostViewedNYT('all-sections', 3)
+    articlesBySection = aFilter.filterBySection(mostViewedArticles)
     articlesByDate = aFilter.filterByDate(mostViewedArticles)
     context = {'articlesBySection': articlesBySection, 'articlesByDate': articlesByDate}
 
